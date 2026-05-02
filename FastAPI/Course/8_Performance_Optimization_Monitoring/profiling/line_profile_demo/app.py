14414e5c00388951b1ca12cb64bf9a432b1aa57c
Nasrullah Asghar <nasrullahasghar897@gmail.com>
2026-05-02 14:37:23 +0500

    profiling in fastapi

---
+++ FastAPI/Course/8_Performance_Optimization_Monitoring/profiling/line_profile_demo/app.py
@@ -0,0 +1,18 @@
+import time
+from fastapi import FastAPI,Request
+
+app = FastAPI()
+def computation(n:int):
+    res = 0
+    for i in range(n):
+        res +=  (i*2)
+    time.sleep(1)
+    return res
+
+@profile
+def process_data(x:int):
+    return computation(x)
+
+@app.get("/profiling")
+def profiling(a:int):
+    return process_data(a)
